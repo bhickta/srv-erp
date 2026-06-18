2fe9c61bc1d7e730aaa3299b4ed912779c2f8cc3
Nishant Bhickta <nishantbhickta@gmail.com>
2026-06-18 13:07:20 +0000

    test(package-barcode): cover barcode settings rules

--- srv_erp/tests/test_package_barcode.py
+++ srv_erp/tests/test_package_barcode.py
@@ -4,8 +4,11 @@ from erpnext.tests.utils import ERPNextTestSuite
 
 from srv_erp.package_barcode.api import generate_package_barcodes, scan_package_barcode
 from srv_erp.package_barcode.service import (
+	DEFAULT_BARCODE_NAMING_SERIES,
 	PackageBarcodeError,
 	PackageBarcodeTransactionValidator,
+	QTY_RULE_ALLOW_MANUAL,
+	QTY_RULE_FORCE_BARCODE,
 	get_item_uom_options,
 )
 
@@ -18,6 +21,13 @@ class TestPackageBarcode(ERPNextTestSuite):
 			properties={"is_stock_item": 1, "stock_uom": "Nos"},
 			uoms=[{"uom": "Box", "conversion_factor": 10}],
 		)
+		frappe.db.set_single_value(
+			"Barcode Settings", "package_barcode_naming_series", DEFAULT_BARCODE_NAMING_SERIES
+		)
+		frappe.db.set_single_value(
+			"Barcode Settings", "package_barcode_default_qty_entry_rule", QTY_RULE_FORCE_BARCODE
+		)
+		frappe.db.set_value("Item", self.item.name, "package_barcode_qty_entry_rule", "Default")
 
 	def test_generate_package_barcodes(self):
 		result = generate_package_barcodes(self.item.name, "Nos", 2)
@@ -27,6 +37,13 @@ class TestPackageBarcode(ERPNextTestSuite):
 			frappe.db.count("Package Barcode", {"generation_batch": result.batch}),
 			2,
 		)
+
+	def test_generate_package_barcodes_uses_configured_series(self):
+		frappe.db.set_single_value("Barcode Settings", "package_barcode_naming_series", "TEST-PBC-.#####")
+
+		result = generate_package_barcodes(self.item.name, "Nos", 1)
+
+		self.assertTrue(result.barcodes[0].startswith("TEST-PBC-"))
 
 	def test_scan_package_barcode(self):
 		result = generate_package_barcodes(self.item.name, "Nos", 1)
@@ -75,3 +92,60 @@ class TestPackageBarcode(ERPNextTestSuite):
 		with self.assertRaises(PackageBarcodeError):
 			PackageBarcodeTransactionValidator(doc).validate()
 
+	def test_forced_package_barcode_qty_rejects_manual_qty_mismatch(self):
+		frappe.db.set_single_value(
+			"Barcode Settings", "package_barcode_default_qty_entry_rule", QTY_RULE_FORCE_BARCODE
+		)
+		result = generate_package_barcodes(self.item.name, "Nos", 1)
+		barcode = frappe.db.get_value("Package Barcode", result.barcodes[0], "barcode")
+		doc = frappe._dict(
+			{
+				"doctype": "Delivery Note",
+				"items": [
+					frappe._dict({"idx": 1, "item_code": self.item.name, "qty": 2}),
+				],
+				"package_barcodes": [
+					frappe._dict(
+						{
+							"idx": 1,
+							"package_barcode": result.barcodes[0],
+							"barcode": barcode,
+							"item_code": self.item.name,
+							"uom": "Nos",
+						}
+					),
+				],
+			}
+		)
+
+		with self.assertRaises(PackageBarcodeError):
+			PackageBarcodeTransactionValidator(doc).validate()
+
+	def test_item_can_allow_manual_qty_over_global_forced_rule(self):
+		frappe.db.set_single_value(
+			"Barcode Settings", "package_barcode_default_qty_entry_rule", QTY_RULE_FORCE_BARCODE
+		)
+		frappe.db.set_value("Item", self.item.name, "package_barcode_qty_entry_rule", QTY_RULE_ALLOW_MANUAL)
+		result = generate_package_barcodes(self.item.name, "Nos", 1)
+		barcode = frappe.db.get_value("Package Barcode", result.barcodes[0], "barcode")
+		doc = frappe._dict(
+			{
+				"doctype": "Delivery Note",
+				"items": [
+					frappe._dict({"idx": 1, "item_code": self.item.name, "qty": 2}),
+				],
+				"package_barcodes": [
+					frappe._dict(
+						{
+							"idx": 1,
+							"package_barcode": result.barcodes[0],
+							"barcode": barcode,
+							"item_code": self.item.name,
+							"uom": "Nos",
+						}
+					),
+				],
+			}
+		)
+
+		PackageBarcodeTransactionValidator(doc).validate()
