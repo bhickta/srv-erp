ab1d067a81039f59f5c495e3dc51f413045da633
Nishant Bhickta <nishantbhickta@gmail.com>
2026-08-14 02:31:49 +0000

    perf(masters): index dynamic item approval queries

--- srv_erp/masters/doctype/dynamic_item_request_attribute/dynamic_item_request_attribute.py
+++ srv_erp/masters/doctype/dynamic_item_request_attribute/dynamic_item_request_attribute.py
@@ -1,5 +1,14 @@
+import frappe
 from frappe.model.document import Document
 
 
 class DynamicItemRequestAttribute(Document):
 	pass
+
+
+def on_doctype_update():
+	frappe.db.add_index(
+		"Dynamic Item Request Attribute",
+		["item_attribute", "attribute_value"],
+		"dynamic_item_attribute_value_idx",
+	)
--- srv_erp/masters/doctype/dynamic_item_request_uom/dynamic_item_request_uom.py
+++ srv_erp/masters/doctype/dynamic_item_request_uom/dynamic_item_request_uom.py
@@ -1,5 +1,14 @@
+import frappe
 from frappe.model.document import Document
 
 
 class DynamicItemRequestUOM(Document):
 	pass
+
+
+def on_doctype_update():
+	frappe.db.add_index(
+		"Dynamic Item Request UOM",
+		["parent", "uom"],
+		"dynamic_item_request_uom_idx",
+	)
