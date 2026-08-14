fb65ca8b38d701fa7a6b26215d02e9581354e674
Nishant Bhickta <nishantbhickta@gmail.com>
2026-08-14 02:22:55 +0000

    fix(masters): enforce safe rollout settings in patch

--- srv_erp/patches/disable_bulk_variant_creation.py
+++ srv_erp/patches/disable_bulk_variant_creation.py
@@ -1,5 +1,6 @@
 import frappe
 
+from srv_erp.masters.dynamic_item.configuration import clear_settings_cache
 from srv_erp.masters.setup import setup_masters_module
 
 
@@ -7,4 +8,6 @@ def execute():
 	setup_masters_module()
 	frappe.db.set_single_value("Masters Settings", "allow_bulk_variant_creation", 0)
 	frappe.db.set_single_value("Masters Settings", "enable_dynamic_item_requests", 0)
+	frappe.db.set_single_value("Masters Settings", "enforce_variant_approval", 1)
 	frappe.db.set_single_value("SRV Settings", "auto_create_variants_on_brand_update", 0)
+	clear_settings_cache()
